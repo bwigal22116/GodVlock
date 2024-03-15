1c842411af00d3383190d24e5b8f584f1288ca2e
Bearcat <bwigal2@nastudents.org>
2024-03-14 20:22:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-
+  <iframe src="about:blank" class="access-frame" style="position: absolute; overflow-x: hidden; width: 100%; height: 100%; display: none; border: none; background: #FFF;"></iframe>
